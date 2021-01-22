b049aefb1225a9a38fc024f0c39382676b764175
Abdoulaye Camara <abdoulayecamara20004@gmail.com>
2021-01-22 12:40:04 +0100

    Creation de la classe Acheteur

---
+++ src/modele/Acheteur.java
@@ -0,0 +1,48 @@
+package modele;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "T_ACHETEUR");
+public class Acheteur{
+
+	@Column(name = "Chiffre_Affaire_Acheteur")
+	private Double chiffreAffaireAchat ;
+	
+	@Column(name = "Nombre_Achat_Acheteur")
+	private int nombreAchat;
+	
+	@Column(name = "Type_Acheteur")
+	private int typeAcheteur;
+
+	public Acheteur() {
+		super();
+	}
+
+	public Double getChiffreAffaireAchat() {
+		return chiffreAffaireAchat;
+	}
+
+	public void setChiffreAffaireAchat(Double chiffreAffaireAchat) {
+		this.chiffreAffaireAchat = chiffreAffaireAchat;
+	}
+
+	public int getNombreAchat() {
+		return nombreAchat;
+	}
+
+	public void setNombreAchat(int nombreAchat) {
+		this.nombreAchat = nombreAchat;
+	}
+
+	public int getTypeAcheteur() {
+		return typeAcheteur;
+	}
+
+	public void setTypeAcheteur(int typeAcheteur) {
+		this.typeAcheteur = typeAcheteur;
+	}
+	
+}
